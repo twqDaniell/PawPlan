8b3e5348d6823f9ee7aa4974523ed78268788751
twqDaniell <thewickedqueend@gmail.com>
2024-12-31 00:56:01 +0200

    Create pet type register fragment

--- app/src/main/java/com/example/pawplan/RegisterActivity.kt
+++ app/src/main/java/com/example/pawplan/RegisterActivity.kt
@@ -1,30 +1,28 @@
 package com.example.pawplan
 
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.NavigationUI
 
-class SignInActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     var navController: NavController? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(R.layout.activity_register)
 
         val navHostFragment: NavHostFragment? =
-            supportFragmentManager.findFragmentById(R.id.main_nav_host) as? NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.nav_host_register) as? NavHostFragment
         navController = navHostFragment?.navController
 
         navController?.let { navController ->
             // Navigate to the first fragment (optional)
             if (savedInstanceState == null) { // Avoid navigation on recreation
-                navController.navigate(R.id.signInFormFragment)
+                navController.navigate(R.id.registerName)
             }
         }
     }
