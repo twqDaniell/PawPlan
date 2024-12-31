25c2f1087c1a2e62739c7dff05c75da46bc05659
twqDaniell <thewickedqueend@gmail.com>
2024-12-31 16:02:46 +0200

    Finish more register fragments

--- app/src/main/java/com/example/pawplan/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/pawplan/RegisterPhotoFragment.kt
@@ -8,21 +8,21 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
-class RegisterDetailsFragment : Fragment() {
+class RegisterPhotoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_register_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_register_photo, container, false)
 
         view.findViewById<Button>(R.id.backButton)?.setOnClickListener {
-            findNavController().navigate(R.id.action_registerDetailsFragment_to_registerPetBreedFragment)
+            findNavController().navigate(R.id.action_registerPhotoFragment_to_registerDetailsFragment)
         }
-
-        view.findViewById<Button>(R.id.nameNextButton)?.setOnClickListener {
-            findNavController().navigate(R.id.action_registerDetailsFragment_to_registerPhotoFragment)
-        }
+//
+//        view.findViewById<Button>(R.id.nameNextButton)?.setOnClickListener {
+//            findNavController().navigate(R.id.action_registerPetNameFragment_to_registerPetBreedFragment)
+//        }
 
         return view
     }
