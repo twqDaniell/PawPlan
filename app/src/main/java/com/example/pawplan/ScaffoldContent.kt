dc78fd1d24cfffd82567557aa96cb616b727986e
twqDaniell <thewickedqueend@gmail.com>
2025-01-03 12:20:51 +0200

    Create health screen basics

--- app/src/main/java/com/example/pawplan/ScaffoldContent.kt
+++ app/src/main/java/com/example/pawplan/ScaffoldContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pawplan.health.HealthScreen
 import com.example.pawplan.profile.ProfileScreen
 
 @Composable
