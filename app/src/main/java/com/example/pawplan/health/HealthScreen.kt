313aeff617a292d73dd636962d9b29c5efe78e44
twqDaniell <thewickedqueend@gmail.com>
2025-01-04 15:10:46 +0200

    Header actual details

--- app/src/main/java/com/example/pawplan/health/HealthScreen.kt
+++ app/src/main/java/com/example/pawplan/health/HealthScreen.kt
@@ -2,18 +2,24 @@ package com.example.pawplan.health
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pawplan.models.MainViewModel
 
 @Composable
-fun HealthScreen() {
+fun HealthScreen(mainViewModel: MainViewModel = viewModel()) {
+    val petDetails by mainViewModel.petDetails.collectAsState()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
-        HeaderSection()
+        HeaderSection(petDetails?.petName ?: "Unknown name")
 
         VetInfoSection()
 
