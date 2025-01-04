a4da0e6e20d33d6bffcea296994e288fa51f01c6
twqDaniell <thewickedqueend@gmail.com>
2025-01-04 19:51:40 +0200

    Change button

--- app/src/main/java/com/example/pawplan/health/VaccinationRecordsSection.kt
+++ app/src/main/java/com/example/pawplan/health/VaccinationRecordsSection.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -48,12 +52,12 @@ fun VaccinationRecordsSection(vetVisits: List<VetVisit>, onAddVisit: (VetVisit) 
                 text = "Vet Visits",
                 style = MaterialTheme.typography.titleMedium
             )
-            OutlinedButton(
-                onClick = { showDialog = true  },
-                contentPadding = PaddingValues(0.dp),
-                modifier = Modifier.size(36.dp) // Ensure consistent button size
-            ) {
-                Text("+", style = MaterialTheme.typography.bodyMedium)
+            IconButton(onClick = { showDialog = true }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Allergy",
+                    tint = MaterialTheme.colorScheme.primary
+                )
             }
         }
 
